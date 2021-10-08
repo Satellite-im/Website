f1de7d66e7ad80a3b21e343f0a81c3c890e4a37d
Samir Vimercati <samir.vimercati@knobs.it>
2021-10-08 19:01:49 +0200

    chore: initial commit, landing page desktop (1480 to 1920), missing team section and footer

---
+++ nuxt.config.js
@@ -0,0 +1,144 @@
+export default {
+  ssr: false,
+  target: 'static',
+  head: {
+    title: 'Satellite.im',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'msapplication-TileColor', content: '#ffffff' },
+      { name: 'theme-color', content: '#ffffff' },
+      { name: 'msapplication-TileImage', content: '/ms-icon-144x144.png' },
+    ],
+    link: [
+      {
+        rel: 'apple-touch-icon',
+        sizes: '57x57',
+        href: '/apple-icon-57x57.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '60x60',
+        href: '/apple-icon-60x60.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '72x72',
+        href: '/apple-icon-72x72.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '76x76',
+        href: '/apple-icon-76x76.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '114x114',
+        href: '/apple-icon-114x114.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '120x120',
+        href: '/apple-icon-120x120.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '144x144',
+        href: '/apple-icon-144x144.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '152x152',
+        href: '/apple-icon-152x152.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-icon-180x180.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '96x96',
+        href: '/favicon-96x96.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '192x192',
+        href: '/favicon-192x192.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '192x192',
+        href: '/android-icon-192x192.png',
+      },
+      {
+        rel: 'manifest',
+        href: '/manifest.json',
+      },
+    ],
+  },
+  css: ['~/assets/css/main.css'],
+  plugins: [],
+  components: true,
+  buildModules: [
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/svg',
+    '@nuxt/image',
+  ],
+  modules: ['@nuxtjs/i18n', '@nuxtjs/robots', 'nuxt-viewport'],
+  build: {
+    babel: {
+      compact: false,
+    },
+  },
+  loading: false,
+  i18n: {
+    locales: [{ code: 'en', iso: 'en-US', file: 'en.json' }],
+    defaultLocale: 'en',
+    langDir: '~/locales/',
+  },
+  robots: {
+    UserAgent: '*',
+    Disallow: '/',
+  },
+  image: {
+    dir: 'assets/images',
+  },
+  viewport: {
+    breakpoints: {
+      xs: 320,
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+      '2xl': 1536,
+    },
+
+    defaultBreakpoints: {
+      desktop: 'lg',
+      mobile: 'xs',
+      tablet: 'md',
+    },
+
+    fallbackBreakpoint: 'lg',
+  },
+}
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  mode: 'jit',
+  purge: {
+    content: [],
+  },
+  darkMode: false,
+  theme: {
+    // color names generated using: https://colornamer.robertcooper.me/
+    colors: {
+      white: '#FFFFFF',
+      'burj-khalifa-fountain': '#D4DEE7',
+      'aircraft-white': '#EDF2F6',
+      'extraordinary-abundance-of-tinge': '#E6E6E6',
+      'obsidian-shard': '#040415',
+      'patriot-blue': '#35365C',
+      accolade: '#8193B2',
+      'cool-green': '#39C661',
+      'spandex-green': '#33B257',
+      'retro-blue': '#2D61F7',
+    },
+    fontFamily: {
+      sans: ['DM Sans', 'sans-serif'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  important: '#app',
+}
