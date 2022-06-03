5ae1efa8555271bf4edea5f3f297b3d3039f73ae
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2022-06-03 21:44:21 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1571,14 +1571,22 @@ export default {
               link: 'https://github.com/maljuburi',
             },
             {
-          key: 'grace',
-          image: '/images/team-grace.webp',
-          name: 'Grace Gaspardo',
-          occupation: this.$t('section_team.team.grace_occupation'),
+          key: 'muhammad',
+          image: '/images/team-muhammad.png',
+          name: 'Muhammad Al Juburi',
+          occupation: this.$t('section_team.team.muhammad_occupation'),
           socials: [
             {
+              key: 'linkedin',
+              link: 'https://www.linkedin.com/in/maljuburi',
+            },
+            {
+              key: 'github',
+              link: 'https://github.com/maljuburi',
+            },
+            {
               key: 'website',
-              link: 'https://www.gaspardo.io',
+              link: 'https://github.com/maljuburi',
             },
           ],
         },
