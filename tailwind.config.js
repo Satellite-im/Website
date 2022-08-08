0aadd53e6d9f79163fa9bbe7dd29619f2d2e27fc
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2022-08-09 00:17:27 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,13 @@ module.exports = {
     colors: {
       white: '#FFFFFF',
       black: '#000000',
+      grey: '#999999',
       'burj-khalifa-fountain': '#D4DEE7',
       'extraordinary-abundance-of-tinge': '#E6E6E6',
       'obsidian-shard': '#040415',
       'patriot-blue': '#35365C',
       accolade: '#8193B2',
-      'cool-green': '#999999', // grey instead of green
+      'cool-green': '#39C661',
       'spandex-green': '#33B257',
       'retro-blue': '#2D61F7',
     },
