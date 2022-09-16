1b3fdb5de916b6e1021f0401cd7292063146f3fd
lgmarchi <lucasgmarchi@gmail.com>
2022-09-16 16:51:55 -0300

    test

--- pages/index.vue
+++ pages/index.vue
@@ -21,13 +21,13 @@
                 class="flex md:order-1 w-[11.98rem] z-10 pr-25"
                 provider="netlify"
               />
-            <div class="flex md:order-2 pl-2 md:pl-20">
+            <div class="flex md:order-2 pl-20">
               <a href="https://core.satellite.im">
-                <button type="button" class="text-white rounded-xl bg-cool-green md:text-base hover:bg-spandex-green focus:bg-spandex-green w-24 md:w-48 h-16 md:h-12">{{ $t('try_alpha') }}</button>
+                <button type="button" class="text-white rounded-xl bg-cool-green md:text-base hover:bg-spandex-green focus:bg-spandex-green w-48 h-12">{{ $t('try_alpha') }}</button>
               </a>
             </div>
             <div
-              class="flex md:order-3 items-center w-full h-12 justify-end"
+              class="flex md:order-3 items-center justify-between w-full h-12 lg:justify-start"
             >
               <nav
                 v-if="$viewport.isGreaterThan('md')"
@@ -237,21 +237,19 @@
                     href="https://core.satellite.im"
                     target="_blank"
                     rel="noopener noreferrer"
-                    class="absolute flex items-center justify-center w-36 md:w-56 md:h-12 h-full text-sm font-bold text-center text-white transition duration-300 ease-linear outline-none  rounded-xl bg-cool-green md:text-base hover:bg-spandex-green focus:bg-spandex-green"
+                    class="absolute flex items-center justify-center w-56 h-full text-sm font-bold text-center text-white transition duration-300 ease-linear outline-none  rounded-xl bg-cool-green md:text-base hover:bg-spandex-green focus:bg-spandex-green"
                   >
                     {{ $t('try_alpha') }}
                   </a>
                 </div>
-                <div class="relative ">
                 <a
                   href="https://satelliteim.eo.page/signup"
                   target="_blank"
                   rel="noopener noreferrer"
-                  class="flex items-center justify-center w-36 md:w-56 h-10 mt-4 text-sm font-bold text-center transition duration-300 ease-linear bg-white shadow-lg outline-none  md:w-56 md:h-12 rounded-xl text-retro-blue md:text-base hover:bg-extraordinary-abundance-of-tinge focus:bg-extraordinary-abundance-of-tinge"
+                  class="flex items-center justify-center w-56 h-10 mt-4 text-sm font-bold text-center transition duration-300 ease-linear bg-white shadow-lg outline-none  md:w-52 md:h-12 rounded-xl text-retro-blue md:text-base hover:bg-extraordinary-abundance-of-tinge focus:bg-extraordinary-abundance-of-tinge"
                 >
                   {{ $t('sign_up') }}
                 </a>
-                </div>
               </div>
             </div>
             <div
@@ -523,7 +521,7 @@
                     href="https://core.satellite.im"
                     target="_blank"
                     rel="noopener noreferrer"
-                    class="absolute flex items-center justify-center w-40 h-full text-sm font-bold text-center text-white transition duration-300 ease-linear outline-none  rounded-xl bg-cool-green md:text-base hover:bg-spandex-green focus:bg-spandex-green"
+                    class="absolute flex items-center justify-center w-56 h-full text-sm font-bold text-center text-white transition duration-300 ease-linear outline-none  rounded-xl bg-cool-green md:text-base hover:bg-spandex-green focus:bg-spandex-green"
                   >
                     {{ $t('try_alpha') }}
                   </a>
