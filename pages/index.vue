2cd6c7c631e487cce34c687bdf720b395b72ad72
lgmarchi <lucasgmarchi@gmail.com>
2022-09-16 16:28:58 -0300

    new test top button

--- pages/index.vue
+++ pages/index.vue
@@ -21,7 +21,11 @@
                 class="flex md:order-1 w-[11.98rem] z-10 pr-25"
                 provider="netlify"
               />
-          
+            <div class="flex md:order-2 pl-0">
+              <a href="https://core.satellite.im">
+                <button type="button" class="text-white rounded-xl bg-cool-green md:text-base hover:bg-spandex-green focus:bg-spandex-green w-16 md:w-48 h-20 md:h-12">{{ $t('try_alpha') }}</button>
+              </a>
+            </div>
             <div
               class="flex md:order-3 items-center justify-between w-full h-12 lg:justify-start"
             >
@@ -46,11 +50,6 @@
               </button>
             </div>
           </header>
-            <div class="flex md:order-2 pl-10">
-              <a href="https://core.satellite.im">
-                <button type="button" class="text-white rounded-xl bg-cool-green md:text-base hover:bg-spandex-green focus:bg-spandex-green w-8 md:w-48 h-12">{{ $t('try_alpha') }}</button>
-              </a>
-            </div>
           <div
             v-if="isSidebarOpen && $viewport.isLessThan('lg')"
             class="fixed top-0 left-0 z-10 w-full h-full"
