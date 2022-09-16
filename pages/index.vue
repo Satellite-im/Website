79d474471216d5af53fa2a1fdb266a4b04cc4327
lgmarchi <lucasgmarchi@gmail.com>
2022-09-16 15:28:13 -0300

    Fix button borders

--- pages/index.vue
+++ pages/index.vue
@@ -237,7 +237,7 @@
                     href="https://core.satellite.im"
                     target="_blank"
                     rel="noopener noreferrer"
-                    class="absolute flex items-center justify-center w-full h-full text-sm font-bold text-center text-white transition duration-300 ease-linear outline-none rounded-3xl bg-cool-green md:text-base hover:bg-spandex-green focus:bg-spandex-green"
+                     class="absolute flex items-center justify-center w-56 h-full text-sm font-bold text-center text-white transition duration-300 ease-linear outline-none  rounded-xl bg-cool-green md:text-base hover:bg-spandex-green focus:bg-spandex-green"
                   >
                     {{ $t('try_alpha') }}
                   </a>
@@ -246,7 +246,7 @@
                   href="https://satelliteim.eo.page/signup"
                   target="_blank"
                   rel="noopener noreferrer"
-                  class="flex items-center justify-center w-48 h-10 mt-4 text-sm font-bold text-center transition duration-300 ease-linear bg-grey shadow-lg outline-none md:w-52 md:h-12 rounded-3xl text-white md:text-base hover:bg-extraordinary-abundance-of-tinge focus:bg-extraordinary-abundance-of-tinge"
+                  class="flex items-center justify-center w-48 h-10 mt-4 text-sm font-bold text-center transition duration-300 ease-linear bg-grey shadow-lg outline-none md:w-52 md:h-12 rounded-xl text-white md:text-base hover:bg-extraordinary-abundance-of-tinge focus:bg-extraordinary-abundance-of-tinge"
                 >
                   {{ $t('sign_up') }}
                 </a>
